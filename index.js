31e559a6793c831c72d1bc39dc0c65c68bd76b16
AaronXue0 <her8153101@yahoo.com.tw>
2021-03-23 01:10:42 +0800

    Basic feature setup.
    1. Messaging test.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
-const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
-
-const queue = new Map();
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -24,28 +21,7 @@ client.on("message", async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
-  const serverQueue = queue.get(message.guild.id);
-  if (message.content.startsWith(`${prefix}summon`)) {
-    summon(message, serverQueue);
-    return;
-  } else if (
-    message.content.startsWith(`${prefix}`) &&
-    message.content.length > 1
-  ) {
-    message.channel.send("~");
-    return;
-  } else {
-    message.channel.send("na ni?");
-    message.channel.send(
-      "If you want to call me\n~play URL :play music\n~skip :skip music\n~stop :stop music"
-    );
-  }
+  message.channel.send("na ni?");
 });
-
-async function summon(message, serverQueue) {
-  const voiceChannel = message.member.voiceChannel;
-  var connection = await voiceChannel.join(); //join channel
-  queueContruct.connection = connection;
-}
 
 client.login(token);

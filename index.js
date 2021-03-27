80fb91a37745dc78075c46c506ddcb1dafa89265
AaronXue0 <her8153101@yahoo.com.tw>
2021-03-27 23:14:57 +0800

    Seperate commands string to json file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
+const { commands: c } = require("./commands.json");
 
 const client = new Discord.Client();
 
@@ -21,17 +22,43 @@ client.on("message", async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
-  if (message.content.startsWith(`${prefix} transfer`)) {
+  if (message.content.startsWith(`${prefix} ` + c.transfer)) {
     Transfer(message);
+  }
+  if (message.content.startsWith(`${prefix} ` + c.fcdis)) {
+    DisconnectSomeOne(message);
   }
 });
 
 async function Transfer(message) {
-  let roleName = message.content.split(`${prefix} transfer `)[1];
+  let roleName = message.content.split(`${prefix} ` + c.transfer + " ")[1];
+  // console.log(message.guild.roles.cache);
+
   let role = message.member.guild.roles.cache.find(
     (role) => role.name === roleName
   );
   if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
+}
+
+async function DisconnectSomeOne(message) {
+  let targetName = message.content.split(`${prefix} ` + c.fcdis + " ")[1];
+
+  let n = "";
+  client.users.cache.forEach((user) => {
+    if (user.username == targetName) {
+      n = user;
+    }
+  });
+  console.log();
+  let member = await message.guild.members.fetch(n.id);
+
+  let id = member.user.id;
+  message.guild.member(id).voice.setChannel(null);
+
+  // Other way to disconnect user
+  // {member.voice.setChannel(null).catch((err) => console.log(err));}
+  // {message.member.voice.setChannel(null).catch((err) => console.log(err));}
+  // {if (targetName) message.guild.member(targetName).voice.setChannel(null);}
 }
 
 client.login(token);
